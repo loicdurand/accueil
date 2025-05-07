cae3aab3ca237ffa991335855253d84ab4b4ab11
Loïc DURAND <loicdurand.ld@gmail.com>
2025-05-07 15:50:03 -0400

    Traduction minime

--- src/Form/RegistrationForm.php
+++ src/Form/RegistrationForm.php
@@ -18,14 +18,14 @@ class RegistrationForm extends AbstractType
     {
         $builder
             ->add('login')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            // ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
